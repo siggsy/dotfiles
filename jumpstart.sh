928ca08efda94611c413e89765bef54c4864d4a3
Žiga Leskovec <zigaleskovec@gmail.com>
2021-03-13 16:57:16 +0100

    Install script added

---
+++ jumpstart.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Copy dotfiles to home folder
+cp -rf ./* ~/
+
+# Setup oh-my-zsh
+chsh -s $(which zsh)
+git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Vim plug installation
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
