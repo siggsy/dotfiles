99797f9524be9d4183d55f94d10bdde1023365cc
siggsy <42301860+siggsy@users.noreply.github.com>
2021-03-14 21:22:09 +0100

    Update jumpstart.sh

--- jumpstart.sh
+++ jumpstart.sh
@@ -12,3 +12,7 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 # Vim plug installation
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+# Add MesloLGS NF Regular font to fonts
+mkdir /usr/share/fonts/meslo
+curl https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf > /usr/share/fonts/meslo/MesloLGS\ NF\ Regular.ttf
